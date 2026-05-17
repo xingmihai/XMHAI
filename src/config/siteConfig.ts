90f1c1299076bb676023c529e1e8da62a3c3f05f
星觅海 <1498934815@qq.com>
2026-05-18 03:39:50 +0800

    Update siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -7,10 +7,10 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "星觅海",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "blog",
 
 	// 站点 URL
 	site_url: "https://firefly.cuteleaf.cn",
